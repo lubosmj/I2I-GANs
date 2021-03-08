8a6bdf336feef430d63169fbf8458ed28bd83377
Lubos Mjachky <lubos.mjachky@gmail.com>
2021-03-08 19:40:23 +0100

    Revert "Make least squares GAN loss default"
    This reverts commit f9dfc7360702b8ebe4feeefaa9b1235da90f4086.

--- i2i_gans/common/losses.py
+++ i2i_gans/common/losses.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 
 from tensorflow.keras import losses
 
-gan_loss_fn = losses.MSE
+gan_loss_fn = losses.BinaryCrossentropy(reduction=losses.Reduction.NONE, from_logits=True)
 
 
 def get_discriminator_loss(real_outputs, fake_outputs):
     real_loss = gan_loss_fn(tf.ones_like(real_outputs), real_outputs)
     fake_loss = gan_loss_fn(tf.zeros_like(fake_outputs), fake_outputs)
-    return (real_loss + fake_loss) * 0.5
+    return real_loss + fake_loss
